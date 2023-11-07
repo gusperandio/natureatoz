c690f63116096f8e1ddc17fd85ff370adc3948ad
Gustavo <gustavo.sperandio25@gmail.com>
2023-11-07 17:24:51 -0300

    Commit bg fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,20 @@ import styles from "./styles/page.module.css";
 import CardHome from "./components/CardHome";
 import req from '../../public/icons/req.svg';
 export default function Home() {
+  
+  const formatNumber = (num: number): string => {
+    if (num >= 1000000) {
+      return (num / 1000000).toFixed(2) + 'M';
+    } else if (num >= 100000) {
+      return (num / 1000).toFixed(1) + 'k';
+    } else {
+      return num.toString();
+    }
+  }
+  
   return (
+
+
     <div>
       <main className={styles.main}>
         <div className={styles.divBtn}>
@@ -12,7 +25,7 @@ export default function Home() {
               <Image src={req} width={22} height={22} alt="requistion"/>
               <span className={styles.like}>Requests</span>
             </span>
-            <span className={styles.likeCount}>2,050</span>
+            <span className={styles.likeCount}>{formatNumber(14)}</span>
           </button>
         </div>
         <CardHome />
