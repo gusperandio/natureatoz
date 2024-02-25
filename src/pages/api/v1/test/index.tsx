5c08faed7a90fddf61c66ff7221c2c4433190adb
Gustavo Sperandio <GSperandio@pr.sebrae.com.br>
2024-02-25 16:51:10 -0300

    Finished V1.0

---
+++ src/pages/api/v1/test/index.tsx
@@ -0,0 +1,68 @@
+import { analytics, log } from "@/lib/firebase";
+import type { NextApiRequest, NextApiResponse } from "next";
+import unorm from "unorm";
+
+interface InsertDatas {
+  title: string;
+  description: string;
+}
+
+const processLetter = (dados: InsertDatas[]): InsertDatas[] => {
+  try {
+    const uniqueDatas = Array.from(
+      new Set(dados.map((item) => item.title))
+    ).map((title) => dados.find((item) => item.title === title)!);
+
+    const processedDatas = uniqueDatas
+      .sort((a, b) => a.title.localeCompare(b.title))
+      .map((item) => ({
+        ...item,
+        title: capitalizeFirstLetter(item.title),
+        description: capitalizeFirstLetter(correctDot(item.description)),
+        letter: removeAccents(item.title.charAt(0).toUpperCase()),
+      }));
+
+    return processedDatas;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
+
+function correctDot(texto: string): string {
+
+  const regex: RegExp = /[.]\s([a-z])/g;
+
+  function sub(match: string, letra: string): string {
+      return ". " + letra.toUpperCase();
+  }
+  return texto.replace(regex, sub);
+}
+
+const capitalizeFirstLetter = (input: string): string => {
+  return input.charAt(0).toUpperCase() + input.slice(1);
+};
+
+const removeAccents = (input: string): string => {
+  const normalized = unorm.nfkd(input);
+  return normalized.replace(/[\u0300-\u036F]/g, "");
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const requestData =
+      typeof req.body === "string" ? JSON.parse(req.body) : req.body;
+    res
+      .status(200)
+      .json({ success: true, inserts: processLetter(requestData) });
+  } catch (error) {
+    res
+      .status(500)
+      .send(
+        "Error in system, report please in https://natureatoz.com.br/report"
+      );
+  }
+}
--- src/pages/api/v1/upload/index.ts
+++ src/pages/api/v1/upload/index.ts
@@ -11,21 +11,44 @@ interface InsertDatas {
 }
 
 const processLetter = (dados: InsertDatas[]): InsertDatas[] => {
-  const uniqueDatas = Array.from(new Set(dados.map(item => item.title))).map(title => dados.find(item => item.title === title)!);
+  try {
+    const uniqueDatas = Array.from(
+      new Set(dados.map((item) => item.title))
+    ).map((title) => dados.find((item) => item.title === title)!);
 
-  const processedDatas = uniqueDatas
-    .sort((a, b) => a.title.localeCompare(b.title))
-    .map(item => ({
-      ...item,
-      letter: removeAccents(item.title.charAt(0).toUpperCase()),
-    }));
+    const processedDatas = uniqueDatas
+      .sort((a, b) => a.title.localeCompare(b.title))
+      .map((item) => ({
+        ...item,
+        title: capitalizeFirstLetter(item.title),
+        description: capitalizeFirstLetter(correctDot(item.description)),
+        letter: removeAccents(item.title.charAt(0).toUpperCase()),
+      }));
 
-  return processedDatas;
+    return processedDatas;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
+
+function correctDot(texto: string): string {
+
+  const regex: RegExp = /[.]\s([a-z])/g;
+
+  function sub(_: string, letra: string): string {
+      return ". " + letra.toUpperCase();
+  }
+  return texto.replace(regex, sub);
+}
+
+const capitalizeFirstLetter = (input: string): string => {
+  return input.charAt(0).toUpperCase() + input.slice(1);
 };
 
 const removeAccents = (input: string): string => {
   const normalized = unorm.nfkd(input);
-  return normalized.replace(/[\u0300-\u036F]/g, '');
+  return normalized.replace(/[\u0300-\u036F]/g, "");
 };
 
 
